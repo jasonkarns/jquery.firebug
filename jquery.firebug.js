f273dae8a0118ded573d2e53a5ccd65c5f0f61aa
Jason Karns <jason@karns.name>
2009-01-20 19:39:36 +0000

    git-svn-id: http://svn.jasonkarns.com/jquery/firebug/trunk@24 efa666ef-1c56-0410-a9ba-b4aff5134045

--- jquery.firebug.js
+++ jquery.firebug.js
@@ -12,6 +12,10 @@
  * support FbLite firebug.d.console.cmd fallbacks
  * 
  * string formatting doesn't work
+ * 
+ * support console utility methods (open/close, minimize/maximize, clear, run)
+ * 
+ * reconfigure log() behavior
  */
 
 (function ($) {
@@ -26,6 +30,7 @@
         defaults: {
             debug: false,
             lite: {
+				override: false,
 				// absolute or relative (to document) URI
 				src: "http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js",
 				//css: "firebuglite/firebug-lite.css",
@@ -36,127 +41,127 @@
             methods: ["assert", "log", "debug", "info", "warn", "error", "dir", "dirxml", "count", "trace", "group", "groupEnd", "time", "timeEnd", "profile", "profileEnd"]
         },
 
-        mountFirebugLite: function (options, force) {
+        mountFirebugLite: function (options) {
             var settings = $.extend(true, {}, $.firebug.defaults, options);
 
-            // if there's no window console or firebug console, import Firebug Lite
-            if ((!(window.console && window.console.firebug) && settings.debug) || force) {
-                $(document).ready(function () {
-                    var firebug = document.createElement('script');
-                    firebug.setAttribute('src', settings.lite.src);
-                    document.body.appendChild(firebug);
-					(function(){
-                        if (window.firebug && window.firebug.version) {
-                            // Firebug Lite has been imported
-							$.extend(window.firebug.env, settings.lite);
-                            window.firebug.init();
+            $(document).ready(function () {
+				window.firebug = window.firebug || {}
+				$.extend(window.firebug.env, settings.lite);
+                var firebug = document.createElement('script');
+                firebug.setAttribute('src', settings.lite.src);
+                document.body.appendChild(firebug);
+				(function(){
+                    if (window.firebug && window.firebug.version) {
+                        // Firebug Lite has been imported
+						$.extend(window.firebug.env, settings.lite);
+						
+						// @TEMP fix for overwriting window.console
+						if (window.firebug.env.override) {
+							window.console = {"provider": "Firebug Lite"};
+							for (var command in firebug.d.console.cmd) {
+								window.console[command] = firebug.lib.util.Curry(firebug.d.console.run, window, command);
+							};
+							
+							window.onerror = function(_message, _file, _line){
+								firebug.d.console.run('error', firebug.lib.util.String.format('{0} ({1},{2})', _message, firebug.getFileName(_file), _line));
+							};
+						}
+						// #end @TEMP fix
+						
+                        window.firebug.init();
+						
+						// Be sure ALL console methods are available
+						$.firebug.mountConsole(settings);
 
-                            // Open to desired state - window.firebug.win.close();
-                            if (settings.lite.minimized) {
-								window.firebug.win.minimize();
-							}
-							else {
-								window.firebug.win.maximize();
-							}
-                            
-                            // add watchXHR support
-                            $.ajaxSetup({watch: settings.lite.watchXHR});
-                            $.ajax = function (ajax_super) {
-                                return function (options) {
-                                    var opts = $.extend(true, {}, $.ajaxSettings, options);
-                                    var xhr = ajax_super.apply(this, arguments);
-                                    if (opts && opts.watch) {
-                                        window.firebug.watchXHR(xhr);
-                                    }
-                                    return xhr;
-                                };
-                            }($.ajax);
+                        // Open to desired state - window.firebug.win.close();
+                        if (settings.lite.minimized) {
+							window.firebug.win.minimize();
+						}
+						else {
+							window.firebug.win.maximize();
+						}
+                        
+                        // add watchXHR support
+                        $.ajaxSetup({watch: settings.lite.watchXHR});
+                        $.ajax = function (ajax_super) {
+                            return function (options) {
+                                var opts = $.extend(true, {}, $.ajaxSettings, options);
+                                var xhr = ajax_super.apply(this, arguments);
+                                if (opts && opts.watch) {
+                                    window.firebug.watchXHR(xhr);
+                                }
+                                return xhr;
+                            };
+                        }($.ajax);
 
-                            // re-map console commands to Firebug Lite commands
-                            var groupStack = [];
-                            for (var method in settings.methods) {
-                                if (settings.methods.hasOwnProperty(method)) {
-                                    switch (settings.methods[method]) {
-                                    // use log() to form a sort of group
-                                    case "group":
-                                        window.console.group = function () {
-                                            return function () {
-                                                // replace the jQuery object as first argument because its useless when printed in FirebugLite
-                                                var args = $.makeArray(arguments);
-                                                args[0] = "Group Start: ";
-                                                groupStack.push(args);
-                                                window.console.log.apply(window.console, args);
-                                            };
-                                        }();
-                                        break;
-                                    // use log() to form a sort of group
-                                    case "groupEnd":
-                                        window.console.groupEnd = function () {
-                                            return function () {
-                                                var args = groupStack.pop();
-                                                args[0] = "Group End: ";
-                                                window.console.log.apply(window.console, args);
-                                            };
-                                        }();
-                                        break;
-                                    // use log() for assert
-                                    case "assert":
-                                        window.console.assert = function () {
-                                            return function () {
-                                                // drop the 'false' argument
-                                                var args = $.makeArray(arguments);
-                                                args.shift();
-												window.console.error("Assertion Failed: ");
-												if (args.length) {
-													window.console.error.apply(window.console, args);
-												}
-                                            };
-                                        }();
-                                        break;
-                                    default:
-                                        break;
-                                    }
+                        // re-map console commands to Firebug Lite commands
+                        var groupStack = [];
+                        for (var method in settings.methods) {
+                            if (settings.methods.hasOwnProperty(method)) {
+                                switch (settings.methods[method]) {
+                                // use log() to form a sort of group
+                                case "group":
+                                    window.console.group = function () {
+                                        return function () {
+                                            // replace the jQuery object as first argument because its useless when printed in FirebugLite
+                                            var args = $.makeArray(arguments);
+                                            args[0] = "Group Start: ";
+                                            groupStack.push(args);
+                                            window.console.log.apply(window.console, args);
+                                        };
+                                    }();
+                                    break;
+                                // use log() to form a sort of group
+                                case "groupEnd":
+                                    window.console.groupEnd = function () {
+                                        return function () {
+                                            var args = groupStack.pop();
+                                            args[0] = "Group End: ";
+                                            window.console.log.apply(window.console, args);
+                                        };
+                                    }();
+                                    break;
+                                // use log() for assert
+                                case "assert":
+                                    window.console.assert = function () {
+                                        return function () {
+                                            // drop the 'false' argument
+                                            var args = $.makeArray(arguments);
+                                            args.shift();
+											window.console.error("Assertion Failed: ");
+											if (args.length) {
+												window.console.error.apply(window.console, args);
+											}
+                                        };
+                                    }();
+                                    break;
+                                default:
+                                    break;
                                 }
                             }
+                        }
 
-                        }
-                        else {
-                            setTimeout(arguments.callee);
-                        }
-					})();
+                    }
+                    else {
+                        setTimeout(arguments.callee);
+                    }
+				})();
 //                    void (firebug);
-                });
-            }
+            });
         },
 
         mountFirebug: function (options) {
             var settings = $.extend({}, $.firebug.defaults, options);
 			
-		    // create basic console object if it doesn't exist
-		    if (!window.console && !settings.debug) {
-		        window.console = {};
-		        for (var method in settings.methods) {
-					if (settings.methods.hasOwnProperty(method)) {
-						// set a blank function foreach console method to avoid 'function undefined' errors
-						if (!window.console[settings.methods[method]]) {
-							window.console[settings.methods[method]] = (function(){
-								return function(){
-								};
-							});
-						}
-					}
-				}
-		    }
-
             // foreach Firebug method, create an associated jQuery function
             for (var method in settings.methods) {
                 if (settings.methods.hasOwnProperty(method)) {
                     // set a blank function foreach console method to avoid 'function undefined' errors
-                    if (!window.console[settings.methods[method]]) {
-                        window.console[settings.methods[method]] = (function() {
-							return function () {};
-						});
-                    }
+//                    if (!window.console[settings.methods[method]]) {
+//                        window.console[settings.methods[method]] = (function() {
+//							return function () {};
+//						});
+//                    }
                     switch (settings.methods[method]) {
                     case "log":
                     case "debug":
@@ -275,9 +280,46 @@
                     }
                 }
             }
-        }
+        },
+
+		mountConsole: function (options) {
+			var settings = $.extend(true, {}, $.firebug.defaults, options);
+			
+		    // create basic console object if it doesn't exist
+		    if (!window.console) {
+				window.console = {};
+			}
+			
+			// Build Console API fallbacks
+		    for (var method in settings.methods) {
+				if (settings.methods.hasOwnProperty(method) && !window.console[settings.methods[method]]) {
+					if (!settings.debug) {
+						// kill function undefined errors if debugging is disabled
+						window.console[settings.methods[method]] = function(){};
+					} else if(window.opera && window.opera.postError){
+						// use Opera
+						window.console[settings.methods[method]] = window.opera.postError;
+					} else if(method != "log"){
+						// use 3rd party log methods
+						window.console[settings.methods[method]] = window.console.log;
+					} else {
+						window.console[settings.methods[method]] = alert;
+					}
+				}
+			}
+		}
     };
 
-    $.firebug.mountFirebugLite(bootstrap);
+	bootstrap = $.extend(true, {}, $.firebug.defaults, bootstrap);
+
+    // if there's no window console or firebug console, (and debugging is turned on)
+    if ((!window.console || !window.console.firebug) && bootstrap.debug) {
+		// import Firebug Lite
+		$.firebug.mountFirebugLite(bootstrap);
+	} else {
+		// else, be sure all console.* methods exist
+		$.firebug.mountConsole(bootstrap);
+	}
+	
     $.firebug.mountFirebug(bootstrap);
 })(jQuery);
